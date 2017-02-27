3f382ee57a25a0518600fab6752a359e8cdfe7f3
Alan Hawkins <alanpika_au@yahoo.com>
2017-02-28 09:32:13 +1100

    Update Prelude.hs

--- Prelude.hs
+++ Prelude.hs
@@ -1,3 +1,18 @@
+module Benner.Prelude (
+    map, (++), concat, filter,
+    head, last, tail, init, null, length, (!!),
+    foldl,
+    iterate, repeat, replicate, cycle,
+    take, drop, splitAt, takeWhile, dropWhile, span, break,
+    lines, words, unlines, unwords, reverse, and, or,
+    any, all, elem, notElem, lookup,
+    sum, product, maximum, minimum, concatMap, 
+    zip, zip3, zipWith, zipWith3, unzip, unzip3,
+    putChar, putStr, putStrLn, print,
+    getChar, getLine, 
+    readFile, writeFile, appendFile
+)
+
 import Prelude hiding ((>>=),return)
 import qualified Control.Monad as CM
 
@@ -7,5 +22,14 @@ bindIO = (>>=)
 returnIO :: a -> IO a
 returnIO = return 
 
-addIntegers :: Integer -> Integer 
-addIntegers = (+)
+integerAdd :: Integer -> Integer 
+integerAdd = (+)
+
+fold :: (a -> b -> a) -> a -> [b] -> a
+fold f z []     = z
+fold f z (x:xs) = fold f (f z x) xs
+
+map :: (a -> b) -> [a] -> [b]
+map f (x:xs) = f x : map xs
+map f [] = [] 
+
